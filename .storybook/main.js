760ae68b70d0131d8e725ad35fd2f1db2613d320
Derrick Yoo <4998684+derrickyoo@users.noreply.github.comderrickyoo@gmail.com>
2022-02-20 11:18:44 -0800

    Add  addon-controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -14,6 +14,7 @@ module.exports = {
     },
     "@storybook/addon-contexts/register",
     "@storybook/addon-backgrounds/register",
+    "@storybook/addon-controls",
   ],
   framework: "@storybook/react",
   core: {
